6ddda26334601e844ba373f3c76a88a4b427d8c7
KrishnaSonawane8008 <krishnasonawane1624@gmail.com>
2025-07-05 21:16:59 +0530

    Vertex Attributes

--- All Files/Main/src/Application.cpp
+++ All Files/Main/src/Application.cpp
@@ -28,6 +28,24 @@ int main(void)
 
     std::cout << glGetString(GL_VERSION) << std::endl;
 
+    //===================================================================================================
+    float triangle_positions[6] = {
+        -0.5f, -0.5f,
+         0.0f,  0.5f,
+         0.5f, -0.5f
+    };
+    
+    
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), triangle_positions, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    //===================================================================================================
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
@@ -35,11 +53,7 @@ int main(void)
         glClear(GL_COLOR_BUFFER_BIT);
 
 
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f(0.0f, 0.5f);
-        glVertex2f(0.5f, -0.5f);
-        glEnd();
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
 
         /* Swap front and back buffers */
