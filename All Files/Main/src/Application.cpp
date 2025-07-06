b1c94cae920595881e24f586c912599c44b3ef36
KrishnaSonawane8008 <krishnasonawane1624@gmail.com>
2025-07-06 21:09:50 +0530

    shaders in file

--- All Files/Main/src/Application.cpp
+++ All Files/Main/src/Application.cpp
@@ -2,6 +2,48 @@
 #include <GLFW/glfw3.h>
 
 #include<iostream>
+#include <fstream>
+#include<string>
+#include <sstream>
+
+
+
+
+
+struct ShaderSourceProgram {
+    std::string VertexShader;
+    std::string FragmentShader;
+};
+
+static ShaderSourceProgram ParseShader(const std::string& filepath)
+{
+    std::ifstream stream(filepath);
+
+    enum class ShaderType {
+        NONE=-1, VERTEX=0, FRAGMENT=1
+    };
+
+    std::string line;
+    std::stringstream ss[2];
+    ShaderType type = ShaderType::NONE;
+    while (getline(stream, line)) 
+    {
+        if (line.find("#shader") != std::string::npos) 
+        {
+            if (line.find("vertex") != std::string::npos) {
+                type = ShaderType::VERTEX;
+            }
+            else if (line.find("fragment") != std::string::npos) {
+                type = ShaderType::FRAGMENT;
+            }
+        }
+        else {
+            ss[(int)type] << line << "\n";
+        }
+    }
+
+    return { ss[0].str(), ss[1].str() };
+}
 
 
 static unsigned int CompileShader(unsigned int type, const std::string& source)
@@ -75,41 +117,39 @@ int main(void)
     std::cout << glGetString(GL_VERSION) << std::endl;
 
     //===================================================================================================
-    float triangle_positions[6] = {
-        -0.5f, -0.5f,
-         0.0f,  0.5f,
-         0.5f, -0.5f
+    float triangle_positions[] = {
+         -0.5f, -0.5f,
+          0.5f, -0.5f,
+          0.5f,  0.5f,
+         -0.5f,  0.5f
+    };
+    //3--2
+    //|  |
+    //0--1
+    unsigned int indices[] = {
+        0,1,2,
+        2,3,0
     };
     
-    
+
     unsigned int buffer;
     glGenBuffers(1, &buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), triangle_positions, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, 8 * sizeof(float), triangle_positions, GL_STATIC_DRAW);
 
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
 
 
-    std::string VertexShader =
-        "#version 330 core\n"
-        "\n"
-        "layout(location=0) in vec4 position;\n"
-        "void main()\n"
-        "{\n"
-        " gl_Position=position;\n"
-        "}\n";
+    unsigned int ibo;
+    glGenBuffers(1, &ibo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
 
-    std::string FragmentShader =
-        "#version 330 core\n"
-        "\n"
-        "layout(location=0) out vec4 colour;\n"
-        "void main()\n"
-        "{\n"
-        " colour=vec4(1.0, 0.0, 0.0, 1.0);\n"
-        "}\n";
 
-    unsigned int Shader = CreateShader(VertexShader, FragmentShader);
+
+    ShaderSourceProgram ShaderString = ParseShader("res/shaders/Basic.shader");
+    unsigned int Shader = CreateShader(ShaderString.VertexShader, ShaderString.FragmentShader);
     glUseProgram(Shader);
 
 
@@ -121,8 +161,7 @@ int main(void)
         glClear(GL_COLOR_BUFFER_BIT);
 
 
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
